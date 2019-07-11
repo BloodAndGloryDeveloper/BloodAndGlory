17421806359f493427dbfbfa282575c02344cb1f
kualigh <18177114151@163.com>
2019-07-11 11:24:25 +0800

    发布新版本

--- src/main/java/bloodandglory/ModInfo.java
+++ src/main/java/bloodandglory/ModInfo.java
@@ -3,7 +3,7 @@ package bloodandglory;
 public class ModInfo {
     public static final String MOD_ID = "bloodandglory";
     public static final String NAME = "Blood And Glory";
-    public static final String VERSION = "0.0.0";
+    public static final String VERSION = "0.0.1-dev";
 
     public static final String SERVERSIDE = "bloodandglory.common.CommonProxy";
     public static final String CLIENTSIDE = "bloodandglory.client.ClientProxy";
