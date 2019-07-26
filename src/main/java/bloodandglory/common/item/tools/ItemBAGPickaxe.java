75edc471e2a1bdfd5c86ec2ced5b27beba0d0308
kualigh <18177114151@163.com>
2019-07-26 20:13:32 +0800

    修改了@Nullable导包错误，修改gradlew构建

--- src/main/java/bloodandglory/common/item/tools/ItemBAGPickaxe.java
+++ src/main/java/bloodandglory/common/item/tools/ItemBAGPickaxe.java
@@ -2,13 +2,13 @@ package bloodandglory.common.item.tools;
 
 import bloodandglory.client.tab.BAGCreativeTabs;
 import bloodandglory.common.item.ItemBAGMaterial;
-import com.sun.istack.internal.NotNull;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 public class ItemBAGPickaxe extends ItemPickaxe {
@@ -18,7 +18,7 @@ public class ItemBAGPickaxe extends ItemPickaxe {
     }
 
     @Override
-    public void addInformation(ItemStack stack, @NotNull World worldIn, List<String> tooltip, ITooltipFlag flagIn){
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn){
         ToolInformation.addToolInformation(tooltip, this);
         if (this.toolMaterial == ItemBAGMaterial.TOOL_MITHRIL){
             tooltip.add(I18n.format("tooltip.bloodandglory.mithril_tool"));
