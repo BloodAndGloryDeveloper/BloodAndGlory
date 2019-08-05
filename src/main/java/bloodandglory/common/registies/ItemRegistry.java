67ad5c9aa8ae2220aa726fd06c769663914ba207
kualigh <18177114151@163.com>
2019-08-05 22:53:05 +0800

    开始开发0.1.0，添加一种盔甲

--- src/main/java/bloodandglory/common/registies/ItemRegistry.java
+++ src/main/java/bloodandglory/common/registies/ItemRegistry.java
@@ -3,6 +3,8 @@ package bloodandglory.common.registies;
 import bloodandglory.BloodAndGlory;
 import bloodandglory.ModInfo;
 import bloodandglory.common.item.ItemBAGMaterial;
+import bloodandglory.common.item.armor.ItemBAGArmor;
+import bloodandglory.common.item.armor.ItemCottonArmor;
 import bloodandglory.common.item.misc.ItemMisc;
 import bloodandglory.common.item.misc.ItemAlcohol;
 import bloodandglory.common.item.food.ItemBAGFood;
@@ -12,6 +14,7 @@ import net.minecraft.client.renderer.ItemMeshDefinition;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
@@ -23,6 +26,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.registries.IForgeRegistry;
 
 import java.lang.reflect.Field;
@@ -44,12 +48,25 @@ public class ItemRegistry {
     public static final Item MITHRIL_SHOVEL = new ItemBAGShovel(ItemBAGMaterial.TOOL_MITHRIL);
     public static final Item MITHRIL_PICKAXE = new ItemBAGPickaxe(ItemBAGMaterial.TOOL_MITHRIL);
     public static final Item MITHRIL_HOE = new ItemBAGHoe(ItemBAGMaterial.TOOL_MITHRIL);
+
+    public static final Item SPEAR = new ItemSpear();
     //Food
     public static final Item ALCOHOL = new ItemAlcohol();
     public static final Item COOKED_EGG = new ItemBAGFood(4,2.0F,true);
     public static final Item RAW_RICE = new ItemRise(2,0.2F,false).setPotionEffect(new PotionEffect(MobEffects.HUNGER,500,0),0.4F);
     public static final Item RICE = new ItemRise(7,0.5F,false);
     public static final Item BANANA = new ItemBAGFood(3,0.7F,false);
+    //Armor
+    //NOTE:helmet是头盔,chestplate是胸甲,leggings是绑腿,boots是靴子
+    public static final Item COTTON_HELMET = new ItemCottonArmor(EntityEquipmentSlot.HEAD);
+    public static final Item COTTON_CHESTPLATE = new ItemCottonArmor(EntityEquipmentSlot.CHEST);
+    public static final Item COTTON_LEGGINGS = new ItemCottonArmor(EntityEquipmentSlot.LEGS);
+    public static final Item COTTON_BOOTS = new ItemCottonArmor(EntityEquipmentSlot.FEET);
+    //todo:待添加这个盔甲的类
+    /*public static final Item MITHRIL_HELMET = new ItemBAGArmor(ItemBAGMaterial.ARMOR_MITHRIL,0, EntityEquipmentSlot.HEAD);
+    public static final Item MITHRIL_CHESTPLATE = new ItemBAGArmor(ItemBAGMaterial.ARMOR_MITHRIL,0,EntityEquipmentSlot.CHEST);
+    public static final Item MITHRIL_LEGGINGS = new ItemBAGArmor(ItemBAGMaterial.ARMOR_MITHRIL,0,EntityEquipmentSlot.LEGS);
+    public static final Item MITHRIL_BOOTS = new ItemBAGArmor(ItemBAGMaterial.ARMOR_MITHRIL,0,EntityEquipmentSlot.FEET);*/
 
     public static void preInit(){
         try {
@@ -70,6 +87,11 @@ public class ItemRegistry {
         item.setRegistryName(idName).setTranslationKey(ModInfo.MOD_ID + "." + idName);
     }
 
+    private static void oreRegistry(){
+        OreDictionary.registerOre("mithril", ItemMisc.EnumItemMisc.MITHRIL.create(1));
+        OreDictionary.registerOre("bronze", ItemMisc.EnumItemMisc.BRONZE.create(1));
+    }
+
     @SubscribeEvent
     public static void itemRegister(final RegistryEvent.Register<Item> event){
         final IForgeRegistry<Item> registry = event.getRegistry();
@@ -79,6 +101,7 @@ public class ItemRegistry {
         for (Item item : ITEMS){
             registry.register(item);
         }
+        oreRegistry();
     }
 
     @SubscribeEvent
